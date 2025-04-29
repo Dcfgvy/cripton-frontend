94d12cff07566531fc566540dc3e970b210ccb06
Ivan <ipkusliy@gmail.com>
2025-04-29 21:00:17 +0300

    small design changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Card } from 'primeng/card';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [Card],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  readonly creationYear = 2025;
+  readonly year = new Date().getFullYear();
+  readonly serviceName = environment.serviceName;
 }
